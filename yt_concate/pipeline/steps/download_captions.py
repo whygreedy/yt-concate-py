468b87f83554828a39b049ed2d71a44b5cc7ee1e
Cindy Yeh <cindyyeah9@gmail.com>
2023-12-05 12:23:14 +0800

    successfully download all video's captions from a channel and write captions to a text file for each video

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,25 +1,28 @@
 import os
-from yt_concate.settings import CAPTIONS_DIR
 
 from pytubefix import YouTube
 
 from yt_concate.pipeline.steps.step import Step
 from yt_concate.utils import Utils
+from yt_concate.settings import CAPTIONS_DIR
 
 
 class DownloadCaptions(Step):
     def process(self, data, inputs):
-        video_link = 'https://www.youtube.com/watch?v=ZRHuv74T4Z8'
-        yt = YouTube(video_link)
-        yt_id = video_link.split("?v=")[1]
-        filepath = os.path.join(CAPTIONS_DIR, yt_id + '.txt')
+        for video_link in data:
+            yt = YouTube(video_link)
+            yt_id = video_link.split("?v=")[1]
+            filepath = os.path.join(CAPTIONS_DIR, yt_id + '.txt')
 
-        subtitles = yt.captions.all()
-        first_subtitle = subtitles[0]
-        first_subtitle_code = first_subtitle.code
+            subtitles = yt.captions.all()
+            try:
+                first_subtitle = subtitles[0]
+                first_subtitle_code = first_subtitle.code
 
-        caption = yt.captions.get_by_language_code(first_subtitle_code)
-        caption_srt = caption.generate_srt_captions()
+                caption = yt.captions.get_by_language_code(first_subtitle_code)
+                caption_srt = caption.generate_srt_captions()
 
-        with open(filepath, 'w') as f:
-            f.write(caption_srt)
+                with open(filepath, 'w') as f:
+                    f.write(caption_srt)
+            except IndexError:
+                continue
